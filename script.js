14ad1398d0b59082413373652f0adc5f4620ddb1
Alexis Arias <alexisariaspro1@gmail.com>
2024-03-19 13:36:54 +0100

    html et JS

---
+++ script.js
@@ -0,0 +1,14 @@
+function shareCard() {
+  // Génère le QR-code
+  alert("QR-code généré ou lien de partage copié !");
+}
+
+function contactBy(contactDetails) {
+  // Rediriger vers le contac tspécifié
+  window.location.href = contactDetails;
+}
+
+function downloadVCard() {
+  // Logique pour télécharger le vCard
+  alert("Téléchargement du vCard lancé !");
+}
